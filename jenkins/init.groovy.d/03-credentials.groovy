69cc14ed0ac06cba69574d69492a5c9611bbee02
JMAR <jeffrey.marron@storable.com>
2025-06-21 07:15:24 -0400

    added dt credential injection

--- jenkins/init.groovy.d/03-credentials.groovy
+++ jenkins/init.groovy.d/03-credentials.groovy
@@ -3,32 +3,21 @@ import jenkins.model.*
 import com.cloudbees.plugins.credentials.*
 import com.cloudbees.plugins.credentials.domains.*
 import com.cloudbees.plugins.credentials.impl.*
-import org.jenkinsci.plugins.plaincredentials.impl.*
 
 def instance = Jenkins.getInstance()
 def domain = Domain.global()
 def store = instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getStore()
 
-// Add Dependency Track API Key credential
-def dtApiKey = new StringCredentialsImpl(
+// Get API key from environment variable
+def dtApiKey = System.getenv('DT_API_KEY') ?: 'odt_admin_api_key_placeholder'
+
+def dtApiKeyCredential = new StringCredentialsImpl(
     CredentialsScope.GLOBAL,
     "dt-api-key",
     "Dependency Track API Key",
-    "odt_admin_api_key_placeholder"  // This will be replaced during setup
+    dtApiKey
 )
 
-store.addCredentials(domain, dtApiKey)
-
-// Add GitHub credentials for private repos (if needed)
-def githubCredentials = new UsernamePasswordCredentialsImpl(
-    CredentialsScope.GLOBAL,
-    "github-credentials",
-    "GitHub Access",
-    "github-user",
-    "github-token"
-)
-
-store.addCredentials(domain, githubCredentials)
-
+store.addCredentials(domain, dtApiKeyCredential)
 instance.save()
-println "Credentials configured successfully"
+println "Credentials configured successfully with API key: ${dtApiKey.take(10)}..."
